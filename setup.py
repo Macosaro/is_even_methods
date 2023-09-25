eda0a49f1eefc43507e47824438abd44e56d1696
Maco <84276894+Macosaro@users.noreply.github.com>
2023-09-25 15:55:33 +0300

    h

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup
 
 from is_even import __version__
 
+with open("README.md") as f:
+    readme = f.read()
+
 setup(
     name="is_even_methods",
     description="A collection of is_even methods",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     version=__version__,
     url="https://github.com/Macosaro/is_even_methods/",
     packages=["is_even"],

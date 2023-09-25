36f7c9f76f9ae7b4a95bf0df137d1cd69c18e06b
Maco <84276894+Macosaro@users.noreply.github.com>
2023-09-25 15:28:04 +0300

    Module description :trollface:

--- is_even/__init__.py
+++ is_even/__init__.py
@@ -1,3 +1,5 @@
+"""A collection of is_even methods"""
+
 from .main import (bitwise_method, convince_method, fast_range_method,
                    hardcoded_method, input_method, intended_method,
                    loop_method, range_method, recursive_method, string_method,
@@ -17,5 +19,5 @@ __all__ = [
     "convince_method",
 ]
 
-__patch_letter__ = "a"
+__patch_letter__ = "b"
 __version__ = str(len(__all__)) + __patch_letter__
